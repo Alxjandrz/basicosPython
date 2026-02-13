259979d143c4a4c53d07878972a4d0ef41d5a6bd
Alxjandrz <omarubio2404@gmail>
2026-02-12 22:04:44 -0800

    Practica 2 de los dias de la semana

--- sesion_03.py
+++ sesion_03.py
@@ -18,3 +18,31 @@ for i in mi_lista:
     for i in mi_lista_2:
         if i == "Lunes":
             print("Feliz", (i))
+
+    # Practica 2 
+    # Dada la lista mi_lista_2 = ["lunes, martes, miercoles, jueves, viernes"]
+    # Imprimir cada elemento de la lista 3 veces y cuando sea lunes no lo incluyas 
+    # Pista: Usalo los dos tipos loop while y for en el mismo programa para lograrlo
+    # Resultado: 
+    # martes
+    # miercoles
+    # jueves 
+    # viernes
+    # martes
+    # miercoles
+    # jueves 
+    # viernes
+    # martes
+    # miercoles
+    # jueves 
+    # viernes
+
+    mi_lista_2 = ["lunes", "martes", "miercoles", "jueves", "viernes"]
+
+i = 0
+
+while i < 3:
+    for dia in mi_lista_2:
+        if dia != "lunes":
+            print(dia)
+    i += 1
