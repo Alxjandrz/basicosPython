158c28713ea57d26158201c9c5c619e69f6453e4
Alxjandrz <omarubio2404@gmail>
2026-02-16 21:26:46 -0800

    Practica 2 dias de la semana

--- sesion_03.py
+++ sesion_03.py
@@ -48,28 +48,4 @@ while i < 3:
     i += 1
 
 
- # PRACTICA 4
 
-    print("======== Mi Super Calculadora ==========")
-
-num_1 = float(input("Escriba el valor del primer número: "))
-num_2 = float(input("Escriba el valor del segundo número: "))
-operacion = input("¿Qué operación deseas hacer? +, -, *, / -> ")
-
-def calculadora(num_1, num_2, operacion):
-    if operacion == "+":
-        return num_1 + num_2
-    elif operacion == "-":
-        return num_1 - num_2
-    elif operacion == "*":
-        return num_1 * num_2
-    elif operacion == "/":
-        if num_2 != 0:
-            return num_1 / num_2
-        else:
-            return "No se puede dividir entre cero"
-    else:
-        return "Accion no valida"
-
-resultado = calculadora(num_1, num_2, operacion)
-print("Resultado:", resultado)
