81eab6ddebadf936671f02bf8daa3889c7f4a4cd
Alxjandrz <omarubio2404@gmail.com>
2026-03-02 15:54:47 -0800

    Modificaciones al minijuego

--- OOP/Ogro.py
+++ OOP/Ogro.py
@@ -1,4 +1,5 @@
 from Enemigo import *
+import random   
 
 class Ogro(Enemigo):
     def __init__(self, puntos_energia=20, ataque=3):
@@ -6,3 +7,10 @@ class Ogro(Enemigo):
 
     def habla(self):
         print("Ogro aplastar todo!!!")
+    
+    def ataque_especial(self):
+        print("Ogro ataque especial")
+        funcion_ataque_especial = random.random() < 0.20
+        if funcion_ataque_especial:
+            self.ataque += 4
+            print("Ogro enojado e incremento")
